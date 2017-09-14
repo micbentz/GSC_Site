dd07608f486f46108829de5a94b6d0a9431acd89
mbentz <mbentz@ufl.edu>
2017-09-14 16:43:39 -0400

    Added UF_BLUE const

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log('hello world');
 
+const UF_BLUE = '#00529b'
+
 $(document).ready(function () {
     console.log(`jquery running`);
 
-    $('header').load('./header/header.html');
+    // $('header').load('./header/header.html');
 });
