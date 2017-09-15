cf0a324dcaedbb4f646e440161b2c99d468542eb
mbentz <mbentz@ufl.edu>
2017-09-15 16:46:14 -0400

    Added datepicker

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ console.log('hello world');
 
 const UF_BLUE = '#00529b'
 const MODAL_BACKGROUND = '#808080';
+const GSC_EMAIL = 'gatorsalsaclub@gmail.com';
 
 $(document).ready(function () {
-
-
-
     /**
      * OVERLAY
      */
 
     // Load in the overlay so that it can be shown later
-    $('.overlay-container').load('./request_overlay/request_overlay.html');
+    $('.overlay-container').load('./request_overlay/request_overlay.html', function() {
+        // Create the datepicker
+        $('#datepicker').datepicker();
+    });
 
     // 
     $('.trigger-modal').click(function() {
@@ -27,10 +28,23 @@ $(document).ready(function () {
     // Capturing clicks outside the overlay content
     // to hide the overlay
     $(document).on('click','.overlay', function(event) {
-        console.log(event);
+        // console.log(event);
         if (event.target.className === 'overlay') {
             hideOverlay();
         }
+    });
+
+    // Sending email
+    $('.overlay-container').on('click','#submit', function(event) {
+        event.preventDefault();
+        let organization = $("input[name='org']").val();
+        let request = $("select[name='request']").val();
+        let date = $("input[name='date']").val();
+        let emailBody = $("textarea[name='message']").val();
+        let subject = `${organization} inquiring about ${request} on ${date}`;
+        console.log(subject);
+        // window.location = 'mailto:' + GSC_EMAIL + '?subject=' + subject + '&body=' +   emailBody;
+        hideOverlay();
     });
 
     function showOverlay() {
@@ -38,13 +52,13 @@ $(document).ready(function () {
         $('.overlay-content').slideDown(1000, function() {
             console.log('finished FADING IN');
         });
-        $('.main').hide();
+        // $('.main').hide();
     }
 
     function hideOverlay() {
         $('.overlay-content').slideUp(1000, function() {
             $('.overlay').css('display','none');
         });
-        $('.main').show();
+        // $('.main').show();
     }
 });
