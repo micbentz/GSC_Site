2eca55f788a9de5a9c1d361ab8c36b6ba20304b2
mbentz <mbentz@ufl.edu>
2017-09-15 14:38:36 -0400

    Added request form overlay

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 console.log('hello world');
 
 const UF_BLUE = '#00529b'
+const MODAL_BACKGROUND = '#808080';
 
 $(document).ready(function () {
-    console.log(`jquery running`);
 
-    // $('header').load('./header/header.html');
+
+
+    /**
+     * OVERLAY
+     */
+
+    // Load in the overlay so that it can be shown later
+    $('.overlay-container').load('./request_survey/request_survey.html');
+
+    // 
+    $('.trigger-modal').click(function() {
+       showOverlay();
+    });
+
+    // Hide the overlay
+    $('.overlay-container').on('click','#close',function() {
+        hideOverlay();
+    });
+
+    // Capturing clicks outside the overlay content
+    // to hide the overlay
+    $(document).on('click','.overlay', function(event) {
+        console.log(event);
+        if (event.target.className === 'overlay') {
+            hideOverlay();
+        }
+    });
+
+    function showOverlay() {
+        $('.overlay').css('display','block');
+        $('.overlay-content').slideDown(1000, function() {
+            console.log('finished FADING IN');
+        });
+        $('.main').hide();
+    }
+
+    function hideOverlay() {
+        $('.overlay-content').slideUp(1000, function() {
+            $('.overlay').css('display','none');
+        });
+        $('.main').show();
+    }
 });
